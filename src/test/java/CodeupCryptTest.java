9db4bc6f63d48e603494ba8b0f6f41b57ddcb772
Fer <fernandoza@gmail.com>
2020-10-21 10:31:46 -0500

    feat: add the final test for checking the passwords

--- src/test/java/CodeupCryptTest.java
+++ src/test/java/CodeupCryptTest.java
@@ -10,6 +10,19 @@ public class CodeupCryptTest {
         assertNotEquals(0.0, CodeupCrypt.version, 0);
     }
 
+    @Test
+    public void testIfHashPasswordWorks(){
+        assertEquals("C0d39p", CodeupCrypt.hashPassword("Codeup"));
+        assertEquals("F3r", CodeupCrypt.hashPassword("Fer"));
+        assertEquals("123", CodeupCrypt.hashPassword("123"));
+        assertEquals("124", CodeupCrypt.hashPassword("12a"));
+        assertEquals(" f3r ", CodeupCrypt.hashPassword(" fer "));
+    }
 
+    @Test
+    public void testIfCheckPasswordWorks(){
+        assertTrue(CodeupCrypt.checkPassword("fer", "f3r"));
+        assertFalse(CodeupCrypt.checkPassword("fer", "fer"));
+    }
 
 }
